fe7aef7e75bfd31b734d7711d38064ca5e0c7e28
Herimanjaka Razakasoa <geit.herimanjaka@gmail.com>
2024-11-03 10:01:16 +0300

    correction: mail verification before registration

--- routes/registerRouter.js
+++ routes/registerRouter.js
@@ -8,6 +8,17 @@ const register = express.Router();
 register.post("/", async (req, res) => {
     try {
         const { username, email, role, password } = req.body;
+        const emailInDb = await prisma.user.findFirst({
+            where: { email },
+            select: { email: true }
+        });
+
+        if (emailInDb) {
+            return res.json({ errors: {
+                message :`Email: ${email} is already in use.`
+            } });
+        }
+
         const hashedPassword = await bcrypt.hash(password, 10);
         const newUser = await prisma.user.create({
             data: { username, email, role, password: hashedPassword },
@@ -20,6 +31,7 @@ register.post("/", async (req, res) => {
 
         res.json(newUser);
     } catch (error) {
+        console.log(error);
         res.status(500).json({
             errors: {
                 message: error.message ?? "Server error, try later."
