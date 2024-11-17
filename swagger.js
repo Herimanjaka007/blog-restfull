01d34fe47d7e831e6bbf287a9281c4c3ee75906a
Herimanjaka Razakasoa <geit.herimanjaka@gmail.com>
2024-11-17 20:35:44 +0300

    documentation for all routes.

---
+++ swagger.js
@@ -0,0 +1,28 @@
+import swaggerJSDoc from "swagger-jsdoc";
+
+const swaggerDefinition = {
+    openapi: "3.0.0",
+    info: {
+        title: "Blog Restfull",
+        version: "1.0.0"
+    },
+    servers: [
+        {
+            url: "http://localhost:3000",
+            description: "server for local dev."
+        },
+        {
+            url: "http://localhost:3000",
+            description: "server for prod."
+        },
+    ],
+};
+
+const options = {
+    definition: swaggerDefinition,
+    apis: ["./routes/*.js", "./swaggerComponents.js"],
+};
+
+const swaggerSpec = swaggerJSDoc(options);
+
+export default swaggerSpec;
---
+++ swaggerComponents.js
@@ -0,0 +1,71 @@
+
+/**
+ * @openapi
+ *  components:
+ *      parameters:
+ *       Id:
+ *        in: path
+ *        name: id
+ *        required: true
+ *      schemas:
+ *          BlogPost:
+ *              type: object
+ *              properties:
+ *                  id:
+ *                      type: integer
+ *                      description: unique identifier of a blog
+ *                      example: 1
+ *                  title:
+ *                      type: string
+ *                      description: blog title
+ *                      example: "Java is always popular ?"
+ *                  content:
+ *                      type: string
+ *                      description: content of a blog
+ *                      example: >
+ *                             java is always popluar and an enterprise langage.
+ *                              This is powerfull and verbose.
+ *                  authorId:
+ *                      type: integer
+ *                      description: id of the author the blog
+ *                  createdAt:
+ *                      type: string
+ *                      format: date-time
+ *                      description: date and time of creation
+ *                      example: "2024-11-17T12:34:56.789Z"
+ *          BlogField:
+ *              type: object
+ *              properties:
+ *                  title:
+ *                      type: string
+ *                      description: title of blog
+ *                  content:
+ *                      type: string
+ *                      description: the content of blog
+ *              required: true
+ * 
+ *          ErrorResponse:
+ *              type: object
+ *              properties:
+ *                  message:
+ *                      type: string
+ *                      description: error message
+ *                      example: Internal server error, try later.
+ *          Author:
+ *              type: object
+ *              properties:
+ *                  email:
+ *                      type: string
+ *                      format: email
+ *                  username:
+ *                      type: string
+ *                      example: Myusername
+ *                  password:
+ *                      type: string
+ *      securitySchemes:
+ *          bearerAuth:
+ *              type: http
+ *              scheme: bearer
+ *              bearerFormat: JWT
+ * 
+ * */
